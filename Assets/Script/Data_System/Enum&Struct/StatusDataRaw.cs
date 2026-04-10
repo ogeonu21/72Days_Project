36d1f157895c47f3ebf2696ae62c61c8fb92e02c
ogeonu21 <monkey6635@kyonggi.ac.kr>
2026-04-10 16:14:04 +0900

    데이터 연동 방식 변경
    csv파일 파싱 방식에서 json으로 변경 및 구글스프레드시트를 통한 데이터 관리 방식으로 변경

---
+++ Assets/Script/Data_System/Enum&Struct/StatusDataRaw.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[System.Serializable]
+public class StatusDataRaw
+{
+    public string Type;
+    public string ID;
+    public string Name;
+
+    public int STR;
+    public int DEX;
+    public int CON;
+    public int AttackBonus;
+    public int HpBonus;
+    public int DodgeBonus;
+    public int RangeBonus;
+}
