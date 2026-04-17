9fe037081f09c02d102dab88b894861a3ba5334a
ogeonu21 <monkey6635@kyonggi.ac.kr>
2026-04-17 09:42:40 +0900

    디버그로그 정리 및 아이템 수정
    armorType 속성 추가, 내구성 값 설정

--- Assets/Script/Data_System/Data/Event/Events/MeetingEvent.cs
+++ Assets/Script/Data_System/Data/Event/Events/MeetingEvent.cs
@@ -25,10 +25,12 @@ public class MeetingEvent : BaseEvent
                             GameManager.Instance.ChangeGoodAndEvil(1);
                         }
                     }
+                    Debug.Log($"<color=cyan>[Event] </color>{who.enemyData.name}과(와) 전투를 시작합니다.");
                     who.successNode = nextNode;
                     NodeManager.Instance.GoToNode(who);
                     break;
                 case meetingEventType.무시하기:
+                    Debug.Log($"<color=cyan>[Event] </color>{who.enemyData.name}을(를) 무시합니다.");
                     NodeManager.Instance.GoToNode(nextNode);
                     break;
                 case meetingEventType.도와주기:
@@ -38,10 +40,11 @@ public class MeetingEvent : BaseEvent
                     {
                         GameManager.Instance.ChangeGoodAndEvil(-1);
                     }
+                    Debug.Log($"<color=cyan>[Event] </color>{who.enemyData.name}을(를) 도와줍니다.");
                     NodeManager.Instance.GoToNode(nextNode);
                     break;
                 default:
-                    Debug.LogWarning("에러에러에러에러에러에러!!!!!!!!!!!!!! meetingEventType 오류!!!!");
+                    Debug.LogWarning($"<color=cyan>[Event] </color>알 수 없는 EventType입니다.");
                     break;
             }
         }
--- Assets/Script/Data_System/Enum&Struct/DataRawStructure/ItemDataRaw.cs
+++ Assets/Script/Data_System/Enum&Struct/DataRawStructure/ItemDataRaw.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [System.Serializable]
 public class ItemDataRaw
 {
+    #region [공통]
     public string ItemID;
     public string ItemName;
     public string ItemDesc;
@@ -10,11 +11,22 @@ public class ItemDataRaw
     public bool Consumable;
     public int ItemValue;
     public string ItemCategory;
+    #endregion
+
+    #region [무기]
     public int Durability; //내구도
     public int AttackBonus;
     public int Range;
+    #endregion
+
+    #region [방어구&악세사리]
+    public string ArmorType;
     public int HpBonus;
     public float DodgeBonus;
     public string QuestID;
+    #endregion
+
+    #region [포션]
     public int Health;
+    #endregion
 }
